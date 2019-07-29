78be8703a59c731c48320542b97195cba8d348d2
Hung Nguyen Xuan <hungnx@pixelz.com>
2019-07-29 21:52:47 +0700

    Init project

---
+++ src/MyClass.java
@@ -0,0 +1,18 @@
+public class MyClass {
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+        //System.out.println("Hello \n\t\"Thu Xinh\"");
+        /*
+        String name="HungDKa";//đây là khai báo biến name
+        int Tuoi=23;
+        System.out.println(name);
+        System.out.println(Tuoi);
+
+        String name1= "Pi";
+        float GiaTri = 3.14f;
+        System.out.println(name1);
+        System.out.println(GiaTri);
+        */
+
+    }
+}
