fb28e3d9936ec1be1831b652d46c02137037eddb
Thu Xinh <thunguyen94.bg@gmail.com>
2019-07-31 23:41:20 +0700

    Thuc hanh ve bien so nguyen va so thuc

--- src/MyClass.java
+++ src/MyClass.java
@@ -15,5 +15,22 @@ public class MyClass {
         */
         int a = 20;
         int b = 30;
+        int c = a+b;
+        System.out.println("Sum:"+c);
+        int d = b - c;
+        System.out.println("subtract:"+d);
+        int e = d * c;
+        System.out.println("muntipal:" + e);
+        double f =(double)a/b;
+        System.out.println("devide:"+ f);
+        //devide:0.6666666865348816bye
+        int b1= 2147483647;
+        int b2 = 1;
+        int b3 = b1+b2;
+        System.out.println(b3);
+
+        float f1 = 34E3f;
+        double d1 = 34E3d;
+        System.out.println("f1 = "+ f1 + ", d1 = "+d1);
     }
 }
